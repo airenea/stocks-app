bf33a517a66382652958c5972ad5ae3c3c7adcb5
Airene Maria <aireneapacible@gmail.com>
2021-11-02 17:10:28 +0800

    Implements single table inheritance

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :admins
+  devise_for :regulars
   root to: 'home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_30_042341) do
+ActiveRecord::Schema.define(version: 2021_11_02_090825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,8 +71,8 @@ ActiveRecord::Schema.define(version: 2021_10_30_042341) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "surname"
-    t.boolean "admin"
     t.boolean "approved"
+    t.string "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
