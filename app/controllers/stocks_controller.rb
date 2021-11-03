6ffdcb436a277f011e5f7282ccc05178b2fa5c74
ivankbautista <ivankevinbautista@gmail.com>
2021-11-03 21:04:08 +0800

    Create functionality to fetch from API

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,2 +1,40 @@
 class StocksController < ApplicationController
+    before_action :get_api, only: [:create, :search]
+    
+    def search
+        @stock = Stock.new
+    end
+
+    def create
+        @stock = Stock.find_or_create_by(symbol: params[:symbol], name: @client.company(stock_params).company_name)
+        @quote = @client.quote(stock_params)
+        
+        @stock.current_price = @quote.latest_price
+        @stock.symbol = @quote.symbol
+        
+        if @stock.save
+            redirect_to stocks_show_path(@stock)
+        else
+        end
+    end
+
+    def show
+        @stock = Stock.find(stock_id)
+    end 
+
+    def get_api
+        @client = IEX::Api::Client.new(
+          publishable_token: 'pk_6db25af117ec4985982e066ca3f76546',
+          secret_token: 'sk_e47cea0714f44c6ea3803835fe18a08c',
+          endpoint: 'https://cloud.iexapis.com/v1'
+        )
+    end
+
+    def stock_params
+        params[:symbol]
+    end
+
+    def stock_id
+        params[:id]
+    end
 end
