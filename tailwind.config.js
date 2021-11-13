feae1f03c1837c20cae2bff651edb62523646e0a
Airene Maria <aireneapacible@gmail.com>
2021-11-13 13:06:15 +0800

    Does all transactions page

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.red,
+      yellow: colors.yellow,
+      green: colors.green,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.purple,
+      teal: colors.teal,
+    }
+  }
+}
