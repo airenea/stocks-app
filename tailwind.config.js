33f20b31e77a9410aa6e93b389e189630d661aef
Airene Maria <aireneapacible@gmail.com>
2021-11-15 21:03:35 +0800

    Attempts to fix styling issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.red,
+      yellow: colors.yellow,
+      green: colors.green,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.purple,
+      teal: colors.teal,
+    }
+  }
 }
