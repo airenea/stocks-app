db62c740d0f481dc1fec92a20b1cdac5f35cb118
Airene Maria <aireneapacible@gmail.com>
2021-11-22 12:57:24 +0800

    Adds validation for models

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
     has_many :user_transactions
     accepts_nested_attributes_for :user_transactions, allow_destroy: true
+    validates :buyer_id, presence:true     
+    validates :stock_id, presence:true         
+    validates :number_of_stocks, numericality: { greater_than_or_equal_to: 0 } 
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
